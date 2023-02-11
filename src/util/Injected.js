91db7bb6e26bd0d43b41b53ffe37fc8014e41527
BunnY <w@bunnywong.com>
2023-02-11 20:52:00 +0800

    comment for error line

--- src/util/Injected.js
+++ src/util/Injected.js
@@ -35,7 +35,7 @@ exports.ExposeStore = (moduleRaidStr) => {
   window.Store.OpaqueData = window.mR.findModule(
     (module) => module.default && module.default.createFromData
   )[0].default
-  window.Store.QueryExist = window.mR.findModule('queryExists')[0].queryExists
+  // window.Store.QueryExist = window.mR.findModule('queryExists')[0].queryExists
   window.Store.QueryProduct = window.mR.findModule('queryProduct')[0]
   window.Store.QueryOrder = window.mR.findModule('queryOrder')[0]
   window.Store.SendClear = window.mR.findModule('sendClear')[0]
